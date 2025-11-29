b829645137964153844ea8c722898a0b30a12582
Benjamín Bracamonte <benjamin.bracamonte@inacapmail.cl>
2025-11-29 18:16:36 -0300

    Commit 2
    Realización de la vista home (catálogo de productos) y detalle del producto, con todas las funcionalidades especificadas en el instructivo de la prueba (filtro, buscador), falta ponerle funcionalidad al botón de solicitar este producto

--- PRUEBA3BACKEND/PRUEBA3BACKEND/urls.py
+++ PRUEBA3BACKEND/PRUEBA3BACKEND/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from mainApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('producto/<int:producto_id>/', views.detalle_producto, name='detalle_producto'),
 ]
 
 if settings.DEBUG:
--- PRUEBA3BACKEND/mainApp/views.py
+++ PRUEBA3BACKEND/mainApp/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from mainApp.models import Producto, Categoria
 
-# Create your views here.
+def home(request):
+    productos = Producto.objects.all()
+    categorias = Categoria.objects.all()
+    
+    categoria_id = request.GET.get('categoria')
+    buscar = request.GET.get("q")
+
+    if categoria_id:
+        productos = productos.filter(categoria_id=categoria_id)
+
+    if buscar:
+        productos = productos.filter(nombre__istartswith=buscar)
+
+    data = {
+        'productos': productos,
+        'categorias': categorias,
+        'categoria_sel': categoria_id,
+        'buscar': buscar
+    }
+    return render(request, 'home.html', data)
+
+def detalle_producto(request, producto_id):
+    producto = Producto.objects.get(id=producto_id)
+    data = {
+        'producto': producto
+    }
+    return render(request, 'detalle_producto.html', data)
