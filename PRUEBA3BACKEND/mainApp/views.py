57db58214e3c19afc67e384ebd6be0a1da79189b
Benjamín Bracamonte <benjamin.bracamonte@inacapmail.cl>
2025-11-30 19:21:05 -0300

    Commit
    Funcionalidad extra realizada (buscar por token en página home) + correcciones al  código mínimas (fecha opcional en formulario) + visualización de imagen en seguimiento del pedido (antes no estaba)

--- PRUEBA3BACKEND/mainApp/views.py
+++ PRUEBA3BACKEND/mainApp/views.py
@@ -1,17 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from mainApp.models import Producto, Categoria, Pedido, ImagenPedido
 from .forms import PedidoForm, ImagenPedidoForm
+from django.core.exceptions import ValidationError
 
 def home(request):
+    error_mensaje = None
+    if request.method == 'POST': 
+        token_buscar = request.POST.get('token_seguimiento')
+        try:
+            Pedido.objects.get(token=token_buscar)
+            return redirect('seguimiento', token=token_buscar)
+        except Pedido.DoesNotExist:
+            error_mensaje = "Token no encontrado. Por favor, verifique e intente nuevamente."
+        except ValidationError:
+            error_mensaje = "Token inválido. Por favor, verifique e intente nuevamente."
+
+        
     productos = Producto.objects.all()
     categorias = Categoria.objects.all()
-    
     categoria_id = request.GET.get('categoria')
     buscar = request.GET.get("q")
 
     if categoria_id:
         productos = productos.filter(categoria_id=categoria_id)
-
     if buscar:
         productos = productos.filter(nombre__istartswith=buscar)
 
@@ -19,7 +30,8 @@ def home(request):
         'productos': productos,
         'categorias': categorias,
         'categoria_sel': categoria_id,
-        'buscar': buscar
+        'buscar': buscar,
+        'error_seguimiento': error_mensaje
     }
     return render(request, 'home.html', data)
 
@@ -45,7 +57,7 @@ def formulario_pedido(request):
         contacto = request.POST.get('cliente_contacto')
         producto_id = request.POST.get('producto')
         descripcion = request.POST.get('descripcion')
-        fecha_necesita = request.POST.get('fecha_necesita')
+        fecha_necesita = request.POST.get('fecha_necesita') or None
 
         pedido = Pedido.objects.create(
             cliente_nombre=nombre,
@@ -77,17 +89,12 @@ def formulario_pedido(request):
         'producto_select': producto,
         'productos': Producto.objects.all()
     }
-
     return render(request, 'formulario_pedido.html', data)
 
 def pedido_confirmacion(request, token):
-    try:
-        pedido = Pedido.objects.get(token=token)
-    except Pedido.DoesNotExist:
-        pedido = None
-
+    pedido = None
     data = {
-        'pedido': pedido
+        'pedido': pedido,
     }
 
     return render(request, 'pedido_confirmacion.html', data)
@@ -98,8 +105,11 @@ def seguimiento(request, token):
     except Pedido.DoesNotExist:
         pedido = None
 
+    imagenes = ImagenPedido.objects.filter(pedido=pedido)
+
     data = {
-        'pedido': pedido
+        'pedido': pedido,
+        'imagenes': imagenes
     }
 
     return render(request, 'seguimiento.html', data)
