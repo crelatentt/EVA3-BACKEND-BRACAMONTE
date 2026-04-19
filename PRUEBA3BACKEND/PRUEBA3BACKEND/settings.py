481623b3e2ce85f8ca48177269fc51e62b4131f0
Benjamín Bracamonte <benjamin.bracamonte@inacapmail.cl>
2026-04-19 17:14:28 -0400

    MultiCloud

--- PRUEBA3BACKEND/PRUEBA3BACKEND/settings.py
+++ PRUEBA3BACKEND/PRUEBA3BACKEND/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-3d$cah8#q#v1ol03*c(!kw81tt%1^7pg&sk1-da!wm+kc_#8e5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'PRUEBA3BACKEND.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pruebaaws',
+        'USER': 'admin',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     },
 }
 
