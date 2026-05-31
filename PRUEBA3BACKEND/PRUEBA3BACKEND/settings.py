ec3385a17389a05c68a1c9b3032f8170a74831af
EC2 Default User <ec2-user@ip-10-0-1-6.ec2.internal>
2026-05-31 22:21:37 +0000

    Preparacion prueba 2 multicloud

--- PRUEBA3BACKEND/PRUEBA3BACKEND/settings.py
+++ PRUEBA3BACKEND/PRUEBA3BACKEND/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -82,10 +83,10 @@ WSGI_APPLICATION = 'PRUEBA3BACKEND.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pruebaaws',
+        'NAME': 'db-multicloud',
         'USER': 'admin',
-        'PASSWORD': 'admin123#',
-        'HOST': 'localhost',
+        'PASSWORD': 'Mwleops1#',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     },
 }
